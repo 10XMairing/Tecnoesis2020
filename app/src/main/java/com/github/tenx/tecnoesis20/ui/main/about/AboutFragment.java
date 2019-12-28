7d2e24757f9c445cb43baeeabb90cba61302381b
nakshatra-bazukaa <nakshatravasugupta@gmail.com>
2019-12-28 15:44:56 +0530

    aboutFragment completed

--- app/src/main/java/com/github/tenx/tecnoesis20/ui/main/about/AboutFragment.java
+++ app/src/main/java/com/github/tenx/tecnoesis20/ui/main/about/AboutFragment.java
@@ -7,17 +7,28 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.github.tenx.tecnoesis20.R;
+import com.google.android.material.tabs.TabLayout;
+
+import butterknife.BindView;
 
 public class AboutFragment extends Fragment {
 
     private AboutViewModel mViewModel;
+    private AboutPageAdapter mAboutPageAdapter;
 
+
+    @BindView(R.id.container)
+    ViewPager mViewPager;
+
+    @BindView(R.id.about_tabs)
+    TabLayout tabLayout;
     public static AboutFragment newInstance() {
         return new AboutFragment();
     }
@@ -25,6 +36,12 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+
+        mAboutPageAdapter = new AboutPageAdapter(getActivity().getSupportFragmentManager());
+
+        setupViewPager(mViewPager);
+        tabLayout.setupWithViewPager(mViewPager);
+
         return inflater.inflate(R.layout.fragment_about, container, false);
     }
 
@@ -33,6 +50,13 @@ public class AboutFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(AboutViewModel.class);
         // TODO: Use the ViewModel
+    }
+
+    private void setupViewPager(ViewPager viewPager){
+        AboutPageAdapter adapter = new AboutPageAdapter(getActivity().getSupportFragmentManager());
+        adapter.addFragment(new SponsorsFragment(), "Sponsors");
+        adapter.addFragment(new TeamsFragment(), "Teams");
+        viewPager.setAdapter(adapter);
     }
 
 }
